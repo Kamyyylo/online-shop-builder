d2bf9cc7a4c32136e28d02fedc00ba1b05a66947
Kamyyylo <kamyszek1@gmail.com>
2019-11-12 19:24:43 +0100

    added frontend build

---
+++ src/main/resources/static/precache-manifest.3c03f3e16db65649962744b122c95294.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45baa9d632848df7d3a878e7fcb3e9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf554762063f6dc7f18",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c0b4fb68d3d773dac63c",
+    "url": "/static/css/main.5e805cf1.chunk.css"
+  },
+  {
+    "revision": "dcf554762063f6dc7f18",
+    "url": "/static/js/2.e4eedf87.chunk.js"
+  },
+  {
+    "revision": "c0b4fb68d3d773dac63c",
+    "url": "/static/js/main.8d53735b.chunk.js"
+  },
+  {
+    "revision": "8c5fc325afea9b4a7f5d",
+    "url": "/static/js/runtime-main.8b8e9234.js"
+  },
+  {
+    "revision": "3a7aaadb22c2eaf93967c9b258b2dd77",
+    "url": "/static/media/fontello.3a7aaadb.woff2"
+  },
+  {
+    "revision": "576e9483c8ed242228b56124656b5c95",
+    "url": "/static/media/fontello.576e9483.eot"
+  },
+  {
+    "revision": "c0cddddc611c832f76f758e45c63846c",
+    "url": "/static/media/fontello.c0cddddc.svg"
+  },
+  {
+    "revision": "cdc2016de7b9f1552db78953c30a897d",
+    "url": "/static/media/fontello.cdc2016d.woff"
+  },
+  {
+    "revision": "d3cd21c2a2c6f09e9596d17de6762920",
+    "url": "/static/media/fontello.d3cd21c2.ttf"
+  }
+]);
